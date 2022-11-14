f47cff8e89d72e53c4e0a60ef614dffa139e974e
Anna-Klimenko <115548478+Anna-Klimenko@users.noreply.github.com>
2022-11-14 21:56:05 +0300

    Update task5.py

--- Лабораторная работа 5/task5.py
+++ Лабораторная работа 5/task5.py
@@ -1,9 +1,8 @@
 import string
 from random import sample
 
-def get_random_password() -> str:
+def get_random_password(n=8) -> str:
     alphabet = string.ascii_uppercase + string.ascii_lowercase + string.digits
-    n = 8
     return ''.join(sample(alphabet,n))
 
 print(get_random_password())
